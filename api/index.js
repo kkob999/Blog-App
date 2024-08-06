8df7351e38620009516fb059493dcf34e96b2d1a
KKob <kob.ruji@gmail.com>
2024-08-06 18:58:25 +0700

    login & logout

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser")
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "adfadfa";
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser())
 
 mongoose.connect(
   "mongodb+srv://kobruji:CyslGMRbrk2jlGI0@cluster0.mpnkyqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -38,13 +40,29 @@ app.post("/login", async (req, res) => {
       if (err) {
         throw err;
       }
-      res.cookie('token', token).json('ok')
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username
+      })
     });
   } else {
     res.status(400).json("wrong credentials");
   }
 //   res.json(passOk);
 });
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies
+    jwt.verify(token, secret, {}, (err, info) => {
+        if(err) throw err
+        res.json(info)
+    })
+    // res.json(req.cookies)
+})
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok')
+})
 
 app.listen(4000);
 
