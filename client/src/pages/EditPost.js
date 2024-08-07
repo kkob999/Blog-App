fc80da689eb6b18e7926beb442bc21e07eb3066e
KKob <kob.ruji@gmail.com>
2024-08-07 17:16:55 +0700

    EditPost(fixed bug)

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -12,6 +12,7 @@ export default function EditPost() {
 
   useEffect(() => {
     fetch("http://localhost:4000/post/" + id).then((response) => {
+        console.log('lll')
       response.json().then((postInfo) => {
         setTitle(postInfo.title);
         setContent(postInfo.content);
